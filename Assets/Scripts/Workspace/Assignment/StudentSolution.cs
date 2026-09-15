b34be21b7034ada26e8ce1caeaeef1e5509c3495
Rahvokkendov <rahvokkendov@gmail.com>
2026-09-15 14:07:22 +0700

    Lecture Done

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -11,16 +11,55 @@ namespace Assignment
         #region Lecture
         public int[] LCT01_SelectionSortAscending(int[] numbers)
         {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i+1; i < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                (numbers[i], numbers[minIndex]) = (numbers[minIndex], numbers[i]);
+
+                //int temp = numbers[minIndex];
+                //numbers[minIndex] = numbers[i];
+                //numbers[i] = temp;
+            }
             return numbers;
         }
 
         public int[] LCT02_BubbleSortAscending(int[] numbers)
         {
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                for(int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+                    }
+                }
+            }
             return numbers;
         }
 
         public int[] LCT03_InsertionSortAscending(int[] numbers)
         {
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+                while (numbers[j] > key && j >= 0)
+                {
+                    numbers[j+1] = numbers[j];
+                    j--;
+                }
+                numbers[j+1] = key;
+                
+            }
             return numbers;
         }
 
@@ -30,16 +69,51 @@ namespace Assignment
 
         public int[] AS01_SelectionSortDescending(int[] numbers)
         {
+            for (int i = 0; i > numbers.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; i > numbers.Length; j++)
+                {
+                    if (numbers[j] > numbers[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                (numbers[i], numbers[minIndex]) = (numbers[minIndex], numbers[i]);
+            }
             return numbers;
         }
 
         public int[] AS02_BubbleSortDescending(int[] numbers)
         {
+            for (int i = 0; i > numbers.Length - 1; i++)
+            {
+                for (int j = 0; j > numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] < numbers[j + 1])
+                    {
+                        (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+                    }
+                }
+            }
             return numbers;
         }
 
         public int[] AS03_InsertionSortDescending(int[] numbers)
         {
+            for (int i = 1; i > numbers.Length; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+                while (numbers[j] < key && j >= 0)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = key;
+
+            }
             return numbers;
         }
 
